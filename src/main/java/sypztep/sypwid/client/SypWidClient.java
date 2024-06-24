244f5afa7561e8bca88a04e0337c950c4bed116b
chai <chainot@hotmail.co.th>
2024-06-24 20:22:20 +0700

    Finish

--- src/main/java/sypztep/sypwid/client/SypWidClient.java
+++ src/main/java/sypztep/sypwid/client/SypWidClient.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.util.Identifier;
 import sypztep.sypwid.client.event.ElytraRenderEvent;
 import sypztep.sypwid.client.event.SpeedOmeterRenderEvent;
-import sypztep.sypwid.client.util.SortType;
 
 public class SypWidClient implements ClientModInitializer {
     public static final String MODID = "sypwid";
