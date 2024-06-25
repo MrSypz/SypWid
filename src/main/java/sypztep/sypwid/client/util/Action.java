e04d66a9c7deba96c57a846deb45f6f54c01ba8b
chai <chainot@hotmail.co.th>
2024-06-25 15:49:42 +0700

    nvm

--- src/main/java/sypztep/sypwid/client/util/Action.java
+++ src/main/java/sypztep/sypwid/client/util/Action.java
@@ -47,51 +47,6 @@ public class Action {
             }
         }
     }
-    public static QuickDepositAction QUICKDEPOSIT = new QuickDepositAction();
-    public static class QuickDepositAction extends ActionHandler {
-        @Override
-        protected void doAction(ScreenHandler screenHandler) {
-            Inventory container = screenHandler.getSlot(0).inventory;
-            List<Slot> slots = screenHandler.slots;
-            int size = slots.size();
-            for (int i = size - 36; i < size - 9; ++i) {
-
-                ItemStack playerInvStack = slots.get(i).getStack();
-                if (container.containsAny(stack -> stack.getItem() == playerInvStack.getItem())) {
-                    screenHandler.quickMove(null, i);
-                }
-            }
-        }
-    }
-    public static RestockAction RESTOCK = new RestockAction();
-    public static class RestockAction implements PlayerActionHandler {
-        @Override
-        public void doAction(ServerPlayerEntity player) {
-            Inventory container = player.currentScreenHandler.getSlot(0).inventory;
-            PlayerInventory inventory = player.getInventory();
-
-            for (int i = 0; i < 36; ++i) {
-                ItemStack stackOne = inventory.getStack(i);
-                int needed = stackOne.getMaxCount() - stackOne.getCount();
-                if (needed > 0) {
-                    for (int j = 0; j < container.size(); ++j) {
-                        ItemStack stackTwo = container.getStack(j);
-                        if (stackOne.getItem() == stackTwo.getItem()) {
-                            int count = stackTwo.getCount();
-
-                            if (needed <= count) {
-                                inventory.insertStack(container.removeStack(j, needed));
-                                needed = 0;
-                            } else {
-                                inventory.insertStack(container.removeStack(j, count));
-                                needed -= count;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
     interface PlayerActionHandler {
         void doAction(ServerPlayerEntity player);
     }
